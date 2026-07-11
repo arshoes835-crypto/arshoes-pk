ae17940194dab67b45fce00e4be8aab96c6f3d45
arshoes835-crypto <arshoes835@gmail.com>
2026-07-11 22:33:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function addToCart(name, price) {
 function orderOnWhatsApp(name, price) {
     let message = "Assalam-o-Alaikum! Main order karna chahta hoon.%0A%0A" +
                   "Product: " + name + "%0A" +
-                  "Price: PKR " + price;
-
+"Price: PKR " + price + "%0A";
     window.open("https://wa.me/923280092911?text=" + message, "_blank");
 }
